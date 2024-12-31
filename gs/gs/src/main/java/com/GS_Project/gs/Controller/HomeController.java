9c3c22b995ccab82506e5a7529d2f8359b1a3bcc
김민기 <94788636+Mingi990408@users.noreply.github.com>
2024-12-31 18:12:53 +0900

    Fix : Server init

---
+++ gs/gs/src/main/java/com/GS_Project/gs/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.GS_Project.gs.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "2048game";
+    }
+}
---
+++ gs/gs/src/test/java/com/GS_Project/gs/GsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GS_Project.gs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
